4454d298d31a1f12f31b7807aae9a74fb2e07139
Lezin901 <l.axmann901@gmail.com>
2025-09-13 17:53:17 +0200

    Dokumentation von Startskript eingebaut und Skript umbenannt zu "rebuild_and_run"

--- rebuild_and_run.sh
+++ rebuild_and_run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-docker rmi boids_simulation
-docker build -t boids_simulation .
+
+# Entfernen von Container, falls dieser vorhanden ist
 docker rm -f boids_simulation
-docker build -t boids_simulation --progress plain .
+
+# Entfernen von altem Image, falls bereits vorhanden
+docker rmi boids_simulation
+
+# Image neu aufbauen anhand von Dockerfile
+docker build -t boids_simulation .
+
+# Container erstellen und laufen lassen
 docker run --rm --name boids_simulation -p 9999:9999 --mount type=bind,source="$(pwd)",target=/home/jovyan -it boids_simulation jupyter lab --port=9999 --notebook-dir=/home/jovyan/
